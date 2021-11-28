c8cb9b51dcbfc5bfeb29f2982dbb3ab56bcde439
Chandra Bose <70272542+chandrabosep@users.noreply.github.com>
2021-11-28 15:31:11 +0530

    Update Password Generator.py

--- GUI Password Generator/Password Generator.py
+++ GUI Password Generator/Password Generator.py
@@ -19,7 +19,7 @@ frame.pack()
 lbl = Label(frame,text="PASSWORD:",font=("bell mt",20))
 lbl.grid(row=0,column=0)
 
-label = Label(frame,text="                   ",font=("ariak",15),bg="#b4d86c")
+label = Label(frame,text="",font=("ariak",15),bg="#b4d86c")
 label.grid(row=0,column=1)
 
 button = Button(root,text="Generate",font =("bell mt",15),command=spin)
